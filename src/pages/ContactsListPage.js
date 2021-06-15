f5bb59424e952a26157695e42ed95bd3f3aa25d3
Emem Isaac <me@ememisaac.com>
2021-06-15 13:05:53 -0700

    Add a page for displaying all contacts

---
+++ src/pages/ContactsListPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import _ from 'lodash';
+import Contact from '../components/Contact';
+
+const Contacts = ({contacts}) => {
+
+  const removeContactHandler = (id) => {
+    setContacts(contacts.filter((contact) => contact.id !== id));
+  }
+
+  return (
+    <>
+      <h2>Contact List</h2>
+      <div className="contacts-list">
+        {
+          _.isEmpty(contacts) ? (<p>No Contacts</p>) : 
+          (contacts.map((contact, key) => (<Book key={key} {...contact} removeContactHandler={removeContactHandler}/>)))   
+        }
+      </div>
+    </>
+  );
+}
+
+export default Contacts;
